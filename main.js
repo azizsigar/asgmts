eb5ce7ee6cf289d7ab325d2a0489f812eb2e3b69
Aziz <aazizsigar@gmail.com>
2023-07-30 12:43:10 +0200

    help

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-const screenDisplay =document.querySelector('.screen')
-const buttons =document.querySelectorAll('button')
-let calculation =[]
-let  accumulativeCalculation
+const dayEl = document.getElementById("day")
+const hourEl = document.getElementById("hour")
+const minuteEl = document.getElementById("minute")
+const secondEl = document.getElementById("second")
+/* buranin altindaki tarihi degistirisen 
+gerisayimi ona gore yapar */
+const newYearTime = new Date("jan 1, 2024 00:00:00").getTime();
 
-function calculate(button){
-    const value = button.textContent
-    if( value == "CLEAR"){
-        calculation = []
-        screenDisplay.textContent = '.'
-    }
-    else if (value == "="){
-        screenDisplay.textContent =eval(accumulativeCalculation)
-    }
-    else{
+updateCountdown()
 
-        calculation.push(value)
-        accumulativeCalculation = calculation.join('')
-        screenDisplay.textContent =  accumulativeCalculation
-    }
+function updateCountdown () {
+    const now = new Date().getTime();
+    const gap = newYearTime - now;
+
+    const second = 1000;
+    const minute = second *60;
+    const hour = minute *60;
+    const day = hour *24;
+
+    
+    const d = Math.floor(gap/day);
+    const h = Math.floor((gap % day)/hour);
+    const m = Math.floor((gap%  hour) /minute);
+    const s = Math.floor((gap % minute) /second);
+    dayEl.innerText = d;
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    setTimeout(updateCountdown, 1000)
 }
-buttons.forEach(button => button.addEventListener('click',() => calculate(button)))
