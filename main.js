4ad88c0911b1e1347821764b6028965fb0ea742c
Aziz <aazizsigar@gmail.com>
2023-07-30 23:02:22 +0200

    asmgt1

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-const dayEl = document.getElementById("day")
-const hourEl = document.getElementById("hour")
-const minuteEl = document.getElementById("minute")
-const secondEl = document.getElementById("second")
-/* buranin altindaki tarihi degistirisen 
-gerisayimi ona gore yapar */
-const newYearTime = new Date("jan 1, 2024 00:00:00").getTime();
-
-updateCountdown()
-
-function updateCountdown () {
-    const now = new Date().getTime();
-    const gap = newYearTime - now;
-
-    const second = 1000;
-    const minute = second *60;
-    const hour = minute *60;
-    const day = hour *24;
-
-    
-    const d = Math.floor(gap/day);
-    const h = Math.floor((gap % day)/hour);
-    const m = Math.floor((gap%  hour) /minute);
-    const s = Math.floor((gap % minute) /second);
-    dayEl.innerText = d;
-    hourEl.innerText = h;
-    minuteEl.innerText = m;
-    secondEl.innerText = s;
-    setTimeout(updateCountdown, 1000)
+const prdt = ["wijn", "citroen", "lime", "tomaten"];
+let i, len, text;
+for (i = 0, len = prdt.length, text = ""; i < len; i++) {
+  text += prdt[i] + "<br>";
 }
+document.getElementById("discount3").innerHTML = text;
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
