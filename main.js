09d537980ab5d162cf36f06962b6317a9481eea1
Aziz <aazizsigar@gmail.com>
2023-08-01 21:43:55 +0200

    tue

--- main.js
+++ main.js
@@ -1,55 +1,80 @@
-/* 1 */ 
-for (let i = 0; i <= 135; i++) {
-    console.log(i);
-} 
-/* 2 */
-/for (let i = 0; i < 135; i= i +2) {
-    console.log(i);
-} 
-/* 3 */
-/for (let i = 1; i < 135; i= i+2) {
-    console.log(i);
-} 
-/* 4 */
+// 1
+let numbers = 0;
 
- const arry =[1,4,2,12];
-for (let i = 0; i < arry.length; i++) {
-    console.log(arry[i]);
-} 
-// 4
- let total = 0;
-for(let i=0; i<135; i++){
-    total += i;
-    console.log(`Number is: ${i} Sum: ${total}`);
-} 
-// 5
-  let arry =[1,4,2,12,-1,-32,1111111111];
-console.log(Math.max(...arry));
-
-} 
-// 6
-let arry =[1,4,2,12,-1,-32,1111111111];
-console.log(Math.min(...arry));
-const grades = [2,1,3];
-
-// 7
-function getAvg(grades) {
-  const total = grades.reduce((acc, c) => acc + c, 0);
-  return total / grades.length;
+for (let i = 200; i <= 2700; i++) {
+  if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
+    numbers += i;
+  }
 }
 
-const average = getAvg(grades);
-console.log(average);
-// 8
-function plusnum(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < 0) {
-        arr[i] = 0;
-      }
-    }
-    return arr;
-  }
+console.log(numbers);
 
-const X = [2, -1, 4, -3];
-const result = plusnum(X);
-console.log(result);
+// 2
+function shiftArrayByOne(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+      return arr;
+    }
+  
+    const shiftedArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      shiftedArray.unshift(arr[i]);
+    }
+  
+    return shiftedArray;
+  }
+  
+  const originalArray = [2, 1, 6, 4, -7];
+  const shiftedArray = shiftArrayByOne(originalArray);
+  console.log(shiftedArray);
+  //3
+  
+//4
+let numbers = [];
+for (let i = 0; i < 135; i++) {
+  if (i % 3 ===0 && i %5 ===0) {numbers.push(i+"fizzBuzz");
+    
+  }else if(i %3===0){
+    numbers.push(i+"fizz")
+
+  }else if (i %5===0){
+    numbers.push(i+"buzz")
+  }else (
+    numbers.push(i)
+  )
+  
+}
+console.log(numbers);
+
+
+//5
+function removenumbers(arr) {
+    return arr.filter((num) => num >= 0);
+  }
+  
+  const ary = [1, -2, 4, 1];
+  const output = removenumbers(ary);
+  
+  console.log(output);
+
+
+// 6
+const inputArray = ['Man', 'I', 'Love', 'The', 'Matrix', 'Program'];
+const cencor = 'Program';
+const outputArray = censorWordInArray(inputArray, cencor);
+
+  
+  function censorWordInArray(arr, cencor) {
+    const censoredArray = arr.map((word) => {
+      if (word === cencor) {
+        return '*'.repeat(word.length);
+      } else {
+        return word;
+      }
+    });
+  
+    return censoredArray;
+  }
+  
+
+  console.log(outputArray);
+  
