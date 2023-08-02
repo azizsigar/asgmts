f49b2e29280d2dbbb48f0baf4ef9fc4cde1f1ae3
aazizsigar <aazizsigar@gmail.com>
2023-08-02 19:19:34 +0200

    wedns

---
+++ projects/main.js
@@ -0,0 +1,9 @@
+function factorilalize(num){
+    if(num<2) {return 1;}
+    let result =1;
+    let currentNumber = Math.floor(num);
+    for (let i=1; i<currentNumber; i++){
+        result=result*1
+    }
+}
+console.log(factorialize(0))
